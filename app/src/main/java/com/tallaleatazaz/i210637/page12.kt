1eef2337aef643aab0930e31e797f4b7cb039ad6
Tallal <mmtallal@gmail.com>
2024-02-13 22:02:53 +0500

    Page 9

---
+++ app/src/main/java/com/tallaleatazaz/i210637/page12.kt
@@ -0,0 +1,11 @@
+package com.tallaleatazaz.i210637
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class page12 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.page5forgotscreen)
+    }
+}
