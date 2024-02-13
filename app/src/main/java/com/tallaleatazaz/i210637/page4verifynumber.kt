535f1acda3b4c28e14330e05617943c89602187e
Tallal <mmtallal@gmail.com>
2024-02-13 21:45:07 +0500

    Page 6

---
+++ app/src/main/java/com/tallaleatazaz/i210637/page4verifynumber.kt
@@ -0,0 +1,11 @@
+package com.tallaleatazaz.i210637
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class page4verifynumber: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.page3signup)
+    }
+}
---
+++ app/src/main/java/com/tallaleatazaz/i210637/page5forgetpassword.kt
@@ -0,0 +1,11 @@
+package com.tallaleatazaz.i210637
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class page5forgetpassword : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.page4verifynumber)
+    }
+}
